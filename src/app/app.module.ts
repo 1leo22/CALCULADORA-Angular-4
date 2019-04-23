9d986d7679570d230cff26af07325c56f7c05b94
Leonardo Souza <leosouzasilva94@gmail.com>
2019-04-23 15:01:38 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TecladoComponent } from './teclado/teclado.component';
+
+import { AlertModule, ButtonsModule } from 'ngx-bootstrap';
+import { InformacoesModule } from './informacoes/informacoes.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TecladoComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AlertModule.forRoot(),
+    ButtonsModule.forRoot(),
+    InformacoesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
