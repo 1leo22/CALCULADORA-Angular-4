d2ee10f5e0d2d28673a6ee2efe889e58d92e9055
Leonardo Souza <leosouzasilva94@gmail.com>
2019-04-23 17:41:14 -0300

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 //Install express server
-const express = require('express');
+/*const express = require('express');
 const path = require('path');
 
 const app = express();
@@ -8,9 +8,23 @@ const app = express();
 app.use(express.static(__dirname + 'src'));
 
 app.get('/*', function(req,res) {
-    
+
 res.sendFile(path.join(__dirname+'/src/app/app.component.html'));
-});
+});    
 
 // Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);*/
+
+
+const express = require('express');
+const path = require('path');
+const nomeApp = process.env.npm_package_name;
+const app = express();
+ 
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+ 
+app.get('/*', (req, res) => {
+res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+});
+ 
 app.listen(process.env.PORT || 8080);
